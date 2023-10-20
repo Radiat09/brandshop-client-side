4444a96030da688f7318681049ba4d1b133ff178
Radiat09 <rzsianboyridoy10@gmail.com>
2023-10-20 18:52:23 +0600

    added a paragraph in featured brand section

--- src/Pages/Home/FeaturedBrands.jsx
+++ src/Pages/Home/FeaturedBrands.jsx
@@ -4,7 +4,10 @@ import PropTypes from "prop-types";
 const FeaturedBrands = ({ brands }) => {
   return (
     <div id="featured_brands" className="max-w-7xl mx-auto my-40">
-      <h2 className="text-4xl font-bold text-center mb-8">Featured Brands</h2>
+      <h2 className="text-4xl font-bold text-center mb-3">Featured Brands</h2>
+      <p className="font-bold text-center mb-5">
+        Microsoft,Google,Intel,Dell,Xiaomi does not have any data
+      </p>
       <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 place-items-center gap-4">
         {brands?.map((brand) => (
           <BrandCard key={brand.id} brand={brand}></BrandCard>
